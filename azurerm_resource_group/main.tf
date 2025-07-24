919f60c4ea2b99d0f123e9068c8d2d9e1d8f7379
RakeshKSingh29 <singhrakesh2986@gmail.com>
2025-07-25 02:01:24 +0530

    stg added

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -3,3 +3,11 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.resource_group_location
 }
 
+resource "azurerm_storage_account" "stg01" {
+  name                     = "stg01classicb35"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  
+}
